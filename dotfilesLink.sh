8ade88e9286c9d4dcec45a100157ee631a920085
Islands5 <five.islands.04010613@gmail.com>
2015-06-21 14:13:38 +0900

    delete install.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
+rm ./install.sh
 mkdir ~/.vimbckup
 mkdir ~/.vimswp
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
